96645e5fd149d3d462b7c86de8841b60c238e8a2
cannedfart <jayromeramirez19@gmail.com>
2026-03-02 08:18:37 -0500

    YESS I GOT THE SPA WORKING LETS GOOOOOOOOOOOOO
    this is legendary cuz it means any and all sound effects can be present without cutoffs or anything of the such

--- meta/spa.js
+++ meta/spa.js
@@ -1,1 +1,23 @@
 // NOT A SPA FOR LIKE UR FACE, THIS IS A SINGLE PAGE APPLICATION, for music & sound effects maybe
+function changeContent(page) {
+	var contentDiv = document.getElementById('content');
+	switch (page) {
+		case 'home':
+			contentDiv.innerHTML = `
+				<!--NOT AI BTW-->
+				<img src="meta/images/logo.png"/>
+				<p>(actually human) website back soon holy shit</p>
+				<h3>by cannedfart <img class="pfp" src="https://cdn.discordapp.com/avatars/1106930666333028422/f676c7fde54ed42351e094cac5825324.png"/> & nebula <img class="pfp" src="https://cdn.discordapp.com/avatars/1407843801762107535/aa55584f743396e1adcd23d949939096.png"> btw :)</h3>
+				<button type="button" class="test-btn" onclick="window.location = '#'; document.getElementById('touch-sound').play(); document.getElementById('decide-sound').play();">test button</button>
+			`;
+			break;
+		case 'guide':
+			contentDiv.innerHTML = `
+				<p>new guide page wowwwww</p>
+				<button type="button" class="test-btn" onclick="changeContent('home'); document.getElementById('touch-sound').play(); document.getElementById('decide-sound').play(); setTimeout(function(){ window.location = 'index.html'; }, 200);">
+					go back home
+				</button>
+			`;
+			break;
+	}
+}
